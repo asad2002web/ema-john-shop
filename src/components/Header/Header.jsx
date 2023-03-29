e681d4e3c78de56ab609f6fb55bb973c2ff07b05
asad2002web <asad2002web@gmail.com>
2023-03-29 14:23:15 +0600

    header component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <nav className="header-menu">
+          <img src={logo} alt="" />
+        <div className="menu-items">
+          <a href="/shop">Shop</a>
+          <a href="/order">Order</a>
+          <a href="/inventory">Inventory</a>
+          <a href="/login">Login</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
